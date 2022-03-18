376df5b497bdb90bad9013dd0b2349af5e9a16e4
Arkadiusz Dziechciarz <arkadiusz.dziechciarz@gmail.com>
2022-03-18 01:03:29 +0100

    controllers, ibactions

---
+++ app-swoosh/Model/Player.swift
@@ -0,0 +1,13 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Arkadiusz Dziechciarz on 18/03/2022.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
